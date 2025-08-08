86d0838451662f9a8a6d9a1fa497a9b6429603a1
Maxim Tresk <djtresk@gmail.com>
2025-08-08 09:01:11 +0500

    refactor: remove unnecessary import in useChatStorage

--- app/composables/useChatStorage.ts
+++ app/composables/useChatStorage.ts
@@ -1,11 +1,10 @@
-import { chatStorage } from '@/utils/chatStorage'
-
 export function useChatStorage() {
     const isInitialized = ref(false)
 
     const initStorage = async () => {
         if (!isInitialized.value) {
             await chatStorage.init()
+
             isInitialized.value = true
         }
     }
